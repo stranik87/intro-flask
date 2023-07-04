72ac53644d9df8f8b4bc0a3a3dffe4f765c64a6c
AZIZBEKRAXMONBERDIYEV <jxechkim079@gmail.com>
2023-07-04 09:22:37 +0500

    add

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask,request , jsonify
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/sum" , methods=['GET','POST'])
 def hello_world():
-    return "<p>Hello!</p>"
+    return "<p>Hello! WORLD</p>"
 
 @app.route("/about")
 def about():
@@ -21,7 +21,23 @@ def hi(name):
 
 @app.route("/number/<int:a>")
 def number(a):
-    return f"<p>hi {a}!</p>"
+    return f"<p>kupaytma {a*100}!</p>"
+
+@app.route("/sum" , methods=['GET','POST'])
+def sum():
+    if request.method=="GET":
+        paramis=request.args
+
+        a=paramis.get("a")
+        b=paramis.get("b")
+
+    elif request.method=="POST":
+        data=request.get_json()
+
+        a=data.get("a")
+        b=data.get("b")
+    s=int(a)+int(b)    
+    return jsonify({"sum":s}) 
 
 
 if __name__ == '__main__':
