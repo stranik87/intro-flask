eb8a163ebf7d28105d40794025b398ebce9de118
AZIZBEKRAXMONBERDIYEV <jxechkim079@gmail.com>
2023-07-05 16:58:59 +0500

    flask

--- main.py
+++ main.py
@@ -6,27 +6,75 @@ app = Flask(__name__)
 
 
 
-@app.route("/sum" , methods=['GET','POST'])
+@app.route("/hello" , methods=['GET','POST'])
 def hello_world():
-    return "<p>Hello! WORLD</p>"
+    if request.method=="GET":
+        return ["Hello World"]
+    elif request.method=="POST":
+        return ["Hello World"]
+    
 
 
-
-@app.route("/sum" , methods=['GET','POST'])
-def sum():
+@app.route("/add" , methods=['GET','POST'])
+def add():
     if request.method=="GET":
         paramis=request.args
-
         a=paramis.get("a")
         b=paramis.get("b")
-
     elif request.method=="POST":
         data=request.get_json()
-
         a=data.get("a")
         b=data.get("b")
     s=int(a)+int(b)    
     return jsonify({"sum":s}) 
+
+
+
+@app.route("/sub" , methods=['GET','POST'])
+def sub():
+    if request.method=="GET":
+        paramis=request.args
+        a=paramis.get("a")
+        b=paramis.get("b")
+    elif request.method=="POST":
+        data=request.get_json()
+        a=data.get("a")
+        b=data.get("b")
+    s=int(a)-int(b)    
+    return jsonify({"cub":abs(s)})
+
+        
+    
+@app.route("/mul" , methods=['GET','POST'])
+def mul():
+    if request.method=="GET":
+        paramis=request.args
+        a=paramis.get("a")
+        b=paramis.get("b")
+    elif request.method=="POST":
+        data=request.get_json()
+        a=data.get("a")
+        b=data.get("b")
+    s=int(a)*int(b)    
+    return jsonify({"mul":s})
+
+
+ 
+@app.route("/div" , methods=['GET','POST'])
+def div():
+    if request.method=="GET":
+        paramis=request.args
+        a=paramis.get("a")
+        b=paramis.get("b")
+    elif request.method=="POST":
+        data=request.get_json()
+        a=data.get("a")
+        b=data.get("b")
+    s=int(a)/int(b)    
+    return jsonify({"div":s}) 
+        
+
+     
 
 
 
