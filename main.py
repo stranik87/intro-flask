5fa4ffee3417412cb3b21dc3c172c5e2c8866211
djumanov <diyorbekjumanov1120@gmail.com>
2023-07-05 17:26:54 +0500

    templates

--- main.py
+++ main.py
@@ -1,82 +1,11 @@
-
-from flask import Flask, request, jsonify
-
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-
-@app.route("/hello" , methods=['GET','POST'])
-def hello_world():
-    if request.method=="GET":
-        return ["Hello World"]
-    elif request.method=="POST":
-        return ["Hello World"]
-    
-
-
-@app.route("/add" , methods=['GET','POST'])
-def add():
-    if request.method=="GET":
-        paramis=request.args
-        a=paramis.get("a")
-        b=paramis.get("b")
-    elif request.method=="POST":
-        data=request.get_json()
-        a=data.get("a")
-        b=data.get("b")
-    s=int(a)+int(b)    
-    return jsonify({"sum":s}) 
-
-
-
-@app.route("/sub" , methods=['GET','POST'])
-def sub():
-    if request.method=="GET":
-        paramis=request.args
-        a=paramis.get("a")
-        b=paramis.get("b")
-    elif request.method=="POST":
-        data=request.get_json()
-        a=data.get("a")
-        b=data.get("b")
-    s=int(a)-int(b)    
-    return jsonify({"cub":abs(s)})
-
-        
-    
-@app.route("/mul" , methods=['GET','POST'])
-def mul():
-    if request.method=="GET":
-        paramis=request.args
-        a=paramis.get("a")
-        b=paramis.get("b")
-    elif request.method=="POST":
-        data=request.get_json()
-        a=data.get("a")
-        b=data.get("b")
-    s=int(a)*int(b)    
-    return jsonify({"mul":s})
-
-
- 
-@app.route("/div" , methods=['GET','POST'])
-def div():
-    if request.method=="GET":
-        paramis=request.args
-        a=paramis.get("a")
-        b=paramis.get("b")
-    elif request.method=="POST":
-        data=request.get_json()
-        a=data.get("a")
-        b=data.get("b")
-    s=int(a)/int(b)    
-    return jsonify({"div":s}) 
-        
-
-     
-
-
+@app.route('/<name>')
+def home(name):
+    return render_template('index.html', title=name, name=name)
 
 if __name__ == '__main__':
     app.run(debug=True)
